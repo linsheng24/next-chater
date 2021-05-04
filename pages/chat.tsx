ec78d27ce1cb0cf345cee04316c886cf08a335c5
linsheng24 <linsheng24@gmail.com>
2021-05-04 22:11:55 +0800

    add chat scroller bar

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,6 +1,8 @@
 import { AppBar, Grid, IconButton, Paper, Toolbar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { AccountCircle } from '@material-ui/icons';
+import { Scrollbars } from 'react-custom-scrollbars';
+import { useEffect, useRef } from 'react';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -20,7 +22,7 @@ const useStyles = makeStyles(theme => ({
       height: '100%',
     },
     appBar: {
-      height: 60,
+      height: '8%',
       display: 'flex',
       alignItems: 'center',
       // @ts-ignore
@@ -31,12 +33,32 @@ const useStyles = makeStyles(theme => ({
     },
     matcherName: {
       margin: 0
-    }
+    },
+    messageContent: {
+      height: '84%',
+    },
+    messageInput: {
+      height: '8%',
+    },
+    test: {
+      height: '30px',
+      margin: 0,
+    },
+    container: {
+      flexDirection: 'column-reverse',
+
+    },
   }),
 );
 
 export default function Chat() {
   const classes = useStyles();
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    scrollRef.current.scrollToBottom();
+  }, []);
+
   return (
     <Grid container justify='center' className={classes.root}>
       <Grid item xs={12} sm={10} md={8}>
@@ -51,6 +73,55 @@ export default function Chat() {
               </IconButton>
             </Toolbar>
           </AppBar>
+          <Grid container className={classes.messageContent}>
+            <Scrollbars ref={scrollRef} className={classes.messageContent}>
+              <Grid container className={classes.container}>
+                <Grid xs={12} className={classes.test}>content1</Grid>
+                <Grid xs={12} className={classes.test}>content2</Grid>
+                <Grid xs={12} className={classes.test}>content3</Grid>
+                <Grid xs={12} className={classes.test}>content4</Grid>
+                <Grid xs={12} className={classes.test}>content5</Grid>
+                <Grid xs={12} className={classes.test}>content6</Grid>
+                <Grid xs={12} className={classes.test}>content7</Grid>
+                <Grid xs={12} className={classes.test}>content8</Grid>
+                <Grid xs={12} className={classes.test}>content9</Grid>
+                <Grid xs={12} className={classes.test}>content10</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+                <Grid xs={12} className={classes.test}>content</Grid>
+              </Grid>
+            </Scrollbars>
+          </Grid>
+          <Grid container className={classes.messageInput}>
+            content
+          </Grid>
         </Paper>
       </Grid>
     </Grid>
